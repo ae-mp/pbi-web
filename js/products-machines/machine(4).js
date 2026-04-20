c31fd04a4e87c94f29f3d33c1f6184f5e57def2c
ae-mp <aem.peralta@gmail.com>
2026-04-20 08:52:11 +0800

    Update machine(4).js

--- js/products-machines/machine(4).js
+++ js/products-machines/machine(4).js
@@ -2,7 +2,7 @@ const machine4 = {
     id: 4,
     name: "ADAM II ",
     tagline: "Image Based Cell Counter",
-    image: "../image/adamii.png",
+    image: "../image/ADAMII.png",
     alt: "adamii_img",
     modalTarget: "#productModal",
     modalProduct: "adamii",
